a6cadfe72c50417ee220e4ba5be4a3c6c8526697
vargha1 <vargha.rahavi@gmail.com>
2024-07-13 00:47:39 +0330

    VR8

--- main.js
+++ main.js
@@ -12,10 +12,8 @@ const loader = new GLTFLoader().setPath("./New/");
 const loader2 = new FontLoader()
 const renderer = new T.WebGLRenderer();
 renderer.setPixelRatio(window.devicePixelRatio);
-renderer.setSize(400, 300);
+renderer.setSize(window.innerWidth, window.innerHeight);
 document.getElementById('canvasHolder').appendChild(renderer.domElement);
-renderer.domElement.classList.add('transition-all')
-renderer.domElement.classList.add('duration-500')
 
 var font = loader2.parse(HelvetikerFont)
 const geo = new TextGeometry("My name is Vargha\nand i am a frontend web\ndeveloper", {
@@ -23,38 +21,37 @@ const geo = new TextGeometry("My name is Vargha\nand i am a frontend web\ndevelo
   size: 0.3,
   depth: 0.1,
   curveSegments: 12,
-
 })
 
 const textMesh = new T.Mesh(geo, new T.MeshPhongMaterial({ color: 0xffffff }))
 textMesh.position.set(-1.3, 4.5, -7)
 scene.add(textMesh)
 
-var fired = false;
-const clickHandler = (el, evt, fn) => el.addEventListener(evt, (e) => {
-  if (!fired) fn(e);
-  fired = true;
-})
+// var fired = false;
+// const clickHandler = (el, evt, fn) => el.addEventListener(evt, (e) => {
+//   if (!fired) fn(e);
+//   fired = true;
+// })
 
-clickHandler(
-  renderer.domElement,
-  'click', () => {
-    renderer.domElement.classList.add('transition-all')
-    renderer.domElement.classList.add('duration-500')
-    document.getElementById('startingText').classList.add('hidden')
-    document.getElementById('header').classList.remove('hidden')
-    document.querySelector("main").classList.add('bottom-0')
-    document.querySelector("main").classList.remove('top-1/2')
-    document.querySelector("main").classList.remove('-translate-y-1/2')
-    renderer.setSize(window.innerWidth, window.innerHeight - 152)
-    renderer.setPixelRatio(window.devicePixelRatio)
-    document.getElementById('canvasHolder').classList.add('w-full')
-    document.getElementById('canvasHolder').classList.remove('w-1/2')
-    controls.autoRotate = false;
-    controls.update()
-    camera.position.set(-12, 8, 0)
-  }
-)
+// clickHandler(
+//   renderer.domElement,
+//   'click', () => {
+//     renderer.domElement.classList.add('transition-all')
+//     renderer.domElement.classList.add('duration-500')
+//     document.getElementById('startingText').classList.add('hidden')
+//     document.getElementById('header').classList.remove('hidden')
+//     document.querySelector("main").classList.add('bottom-0')
+//     document.querySelector("main").classList.remove('top-1/2')
+//     document.querySelector("main").classList.remove('-translate-y-1/2')
+//     renderer.setSize(window.innerWidth, window.innerHeight - 152)
+//     renderer.setPixelRatio(window.devicePixelRatio)
+//     document.getElementById('canvasHolder').classList.add('w-full')
+//     document.getElementById('canvasHolder').classList.remove('w-1/2')
+//     controls.autoRotate = false;
+//     controls.update()
+//     camera.position.set(-12, 8, 0)
+//   }
+// )
 
 const positions = [
   {
@@ -99,21 +96,28 @@ document.querySelectorAll('button').forEach((btn, key) => {
     if (key != 3) {
       gsap.to(camera.position, positions[key])
     } else {
-      fired = false;
-      renderer.domElement.classList.remove('transition-all')
-      renderer.domElement.classList.remove('duration-500')
-      document.getElementById('startingText').classList.remove('hidden')
-      document.getElementById('header').classList.add('hidden')
-      document.querySelector("main").classList.remove('bottom-0')
-      document.querySelector("main").classList.add('top-1/2')
-      document.querySelector("main").classList.add('-translate-y-1/2')
-      renderer.setSize(400, 300)
-      renderer.setPixelRatio(window.devicePixelRatio)
-      document.getElementById('canvasHolder').classList.remove('w-full')
-      document.getElementById('canvasHolder').classList.add('w-1/2')
-      controls.reset()
-      controls.autoRotate = true;
-      controls.update()
+      gsap.to(camera.position, {
+        x: -12,
+        y: 8,
+        z: 0,
+        duration: 2,
+        ease: "none",
+        onUpdate: function () {
+          controls.target = new T.Vector3(0, 0, 0)
+          controls.update()
+        },
+      })
+      // fired = false;
+      // document.querySelector("main").classList.remove('bottom-0')
+      // document.querySelector("main").classList.add('top-1/2')
+      // document.querySelector("main").classList.add('-translate-y-1/2')
+      // renderer.setSize(400, 300)
+      // renderer.setPixelRatio(window.devicePixelRatio)
+      // document.getElementById('canvasHolder').classList.remove('w-full')
+      // document.getElementById('canvasHolder').classList.add('w-1/2')
+      // controls.reset()
+      // controls.autoRotate = true;
+      // controls.update()
     }
   })
 })
@@ -124,16 +128,59 @@ loader.load("scene.gltf", function (gltf) {
   scene.add(mesh)
 })
 
-// const geometry = new T.BoxGeometry(200, 20, 350);
-// const material = new T.MeshPhongMaterial({ color: 0x28282B });
-// const baseCube = new T.Mesh(geometry, material);
-// baseCube.receiveShadow = true
+const geometry = new T.BoxGeometry(50, 5, 50);
+const material = new T.MeshStandardMaterial({ color: 0x000000 });
+const baseCube = new T.Mesh(geometry, material);
+baseCube.receiveShadow = true
+baseCube.position.set(0, -2, 0)
+// baseCube.name = "start"
 // scene.add(baseCube)
-// camera.position.set(180, 90, 230)
-// controls.update()
-// const geometry2 = new T.BoxGeometry(180, 10, 330);
-// const cube2 = new T.Mesh(geometry2, material);
-// scene.add(cube2)
+
+const geo2 = new T.BoxGeometry(1, 1, 1)
+const material2 = new T.MeshStandardMaterial({ color: 0xffffff });
+let firstLayer = [{ name: "next", color: 0x000000 }, { name: "previous", color: 0x000000 }, { name: "", color: 0x000000 }, { name: "", color: 0x000000 }]
+let secondLayer = [{ name: "start", color: 0x00FF00 }, { name: "stop", color: 0xFF0000 }, { name: "forward", color: 0x00FFFF }, { name: "rewind", color: 0xFFA500 }]
+let cubesGroup = new T.Group();
+
+for (let i = 0; i < 2; i++) {
+  let space = 0.3
+  for (let j = 0; j < 4; j++) {
+    let cube = new T.Mesh(geo2, material2);
+    cube = new T.Mesh(geo2, cube.material.clone())
+    cube.position.set(0, 1, j + 12 + space)
+    cube.name = firstLayer[j].name
+    cube.material.color.set(firstLayer[j].color)
+    space += 0.3
+    if (i >= 1) {
+      cube.position.set(0, 2.2, j + 12 + space - 0.3)
+      cube.name = secondLayer[j].name
+      cube.material.color.set(secondLayer[j].color)
+    }
+    cubesGroup.add(cube)
+    scene.add(cubesGroup)
+  }
+}
+
+const videoTexture = new T.VideoTexture(document.getElementById("video"))
+videoTexture.needsUpdate = true;
+const material3 = new T.MeshStandardMaterial({ color: 0xffffff, map: videoTexture, side: T.FrontSide, toneMapped: false });
+material3.needsUpdate = true;
+const geo3 = new T.BoxGeometry(1, 4, 4.9)
+const geo4 = new T.BoxGeometry(0.8, 4.3, 5.2)
+const bigCube = new T.Mesh(geo3, [
+  new T.MeshStandardMaterial({ color: 0xffffff }),
+  material3,
+  new T.MeshStandardMaterial({ color: 0xffffff }),
+  new T.MeshStandardMaterial({ color: 0xffffff }),
+  new T.MeshStandardMaterial({ color: 0xffffff }),
+  new T.MeshStandardMaterial({ color: 0xffffff }),
+]);
+
+const bigCube2 = new T.Mesh(geo4, material);
+bigCube.position.set(0, 5, 14.25)
+bigCube2.position.set(0, 5, 14.25)
+scene.add(bigCube)
+scene.add(bigCube2)
 
 const ambientLight = new T.AmbientLight(0xffffff)
 scene.add(ambientLight)
@@ -171,17 +218,40 @@ dl2.castShadow = true
 scene.add(dl2)
 
 const controls = new OrbitControls(camera, renderer.domElement)
-controls.rotateSpeed = 0.33;
-controls.autoRotate = true
 controls.enablePan = false
-controls.minPolarAngle = 1.12;
-controls.maxPolarAngle = 1.12;
+controls.minPolarAngle = 1;
+controls.maxPolarAngle = 1.2;
 controls.minDistance = 1;
 controls.maxDistance = 100;
 controls.update()
 
+const raycaster = new T.Raycaster()
+
+document.addEventListener('mousedown', onMouseDown)
+
+function onMouseDown(event) {
+  camera.updateProjectionMatrix()
+  controls.update()
+  const coords = new T.Vector2(
+    (event.clientX / renderer.domElement.width) * 2 - 1,
+    -((event.clientY / renderer.domElement.height) * 2 + 1),
+  )
+  raycaster.setFromCamera(coords, camera)
+
+  let intersections = raycaster.intersectObjects(cubesGroup, true);
+  if (intersections.length > 0) {
+    if (intersections[0].object.name == "start") {
+      document.getElementById("video").play();
+      console.log("played");
+    }
+    console.log(intersections[0].object.geometry.parameters)
+  }
+}
+
+
 function animate() {
   renderer.render(scene, camera);
+  camera.updateProjectionMatrix()
   controls.update()
 }
 renderer.setAnimationLoop(animate)
