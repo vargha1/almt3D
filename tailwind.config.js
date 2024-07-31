fa9eb10aaff9e0b9423dbfa65798395e7965caf7
vargha1 <vargha1@users.noreply.github.com>
2024-07-31 07:53:44 +0000

    deploy: bd4fdea0a212377fd4822c66890145b70041b5b3

---
+++ tailwind.config.js
@@ -0,0 +1,9 @@
+/** @type {import('tailwindcss').Config} */
+export default {
+  content: ["./**/*.{html,js}","/form.html" , "form.html", "./public/form.html"],
+  theme: {
+    extend: {},
+  },
+  plugins: [],
+}
+
